08757cfe5b4c4b9c0a567e2910a75a3d2f45b3bb
Fai <fnalswailem@gmail.com>
2022-09-29 17:36:24 +0300

    cache state edited

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -123,7 +123,7 @@ def _load_cache():
 
 def _save_cache(cache):
     # TODO: Save the cache, if it has changed
-    if cache.has_state_changed:
+    if cache.HasStateChanged:
         session['token_cache'] = cache.serialize()
 
 def _build_msal_app(cache=None, authority=None):
