2123fb90571206394ce6288ad97bdb8e6d309ab6
Fai <fnalswailem@gmail.com>
2022-09-29 20:29:19 +0300

    logging1

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -62,12 +62,14 @@ def post(id):
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
+        app.logger.info('Successfull login!')
         return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
+            app.logger.info('Unsuccessfull login!')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
@@ -98,6 +100,7 @@ def authorized():
         # Here, we'll use the admin username for anyone who is authenticated by MS
         user = User.query.filter_by(username="admin").first()
         login_user(user)
+        
         _save_cache(cache)
     return redirect(url_for('home'))
 
