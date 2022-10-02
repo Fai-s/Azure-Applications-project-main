7ea3e5d860e6c17a4c5d11ab3f6431d892fe8639
Fai <fnalswailem@gmail.com>
2022-10-03 01:37:39 +0300

    login success

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -61,7 +61,6 @@ def post(id):
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        app.logger.info("user logged in successfullya")
         return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
@@ -70,18 +69,12 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        app.logger.info("user logged in successfullyb")
         next_page = request.args.get('next')
-        app.logger.info("user logged in successfullyc")
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
-            app.logger.info("user logged in successfullyd")
         return redirect(next_page)
-    app.logger.info("user logged in successfullye")
     session["state"] = str(uuid.uuid4())
-    app.logger.info("user logged in successfullyf")
     auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
-    app.logger.info("user logged in successfullyg")
     return render_template('login.html', title='Sign In', form=form, auth_url=auth_url)
 
 @app.route(Config.REDIRECT_PATH)  # Its absolute URL must match your app's redirect_uri set in AAD
@@ -106,7 +99,7 @@ def authorized():
         user = User.query.filter_by(username="admin").first()
         login_user(user)
         _save_cache(cache)
-    app.logger.info("user logged in successfullyh")
+    app.logger.info("user logged in successfully")
     return redirect(url_for('home'))
 
 @app.route('/logout')
